dc5dca7a67c9dbf866011b8f809e28ff6d18b957
Kimyewon <yewon6282@gmail.com>
2023-04-03 22:15:19 +0900

    init setting

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background: inherit;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyle;
