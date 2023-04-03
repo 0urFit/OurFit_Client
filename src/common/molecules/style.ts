84ac72706210d7ecbeef5cdc41297c03d03db8e4
Kimyewon <yewon6282@gmail.com>
2023-04-04 00:39:03 +0900

    feat: InfoInput Component 추가

--- src/common/molecules/style.ts
+++ src/common/molecules/style.ts
@@ -30,4 +30,26 @@ const LI = {
     `,
 };
 
-export { LI };
+const II = {
+    InputWrapper: styled.div`
+        position: relative;
+        width: 18.375rem;
+        height: 3.1875rem;
+    `,
+
+    Input: styled.input`
+        padding-left: 1.3125rem;
+        width: inherit;
+        height: inherit;
+        font-weight: 400;
+        font-size: 0.875rem;
+        border: 2px solid rgba(232, 232, 232, 0.7);
+        border-radius: 0.625rem;
+
+        &::placeholder {
+            color: #27639d;
+        }
+    `,
+};
+
+export { LI, II };
