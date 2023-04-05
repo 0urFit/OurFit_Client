60d33a7227eed7eabb0d1051bdcd078d4baa74c2
seonjae <tjswo2292@naver.com>
2023-04-05 16:03:01 +0900

    feat: RoutineCard Component 추가

---
+++ src/common/molecules/RoutineCard.tsx
@@ -0,0 +1,41 @@
+import Image from 'next/image';
+import { RC } from './style';
+
+import CardImg from '../../../public/assets/Card-img.jpg';
+import LikeIcon from '../../../public/assets/like-icon.png';
+
+const RoutineCard = () => {
+    return (
+        <RC.CardBox>
+            <RC.CardWrapper>
+                <RC.ImgWrapper>
+                    <Image src={CardImg} alt="운동이미지" />
+                </RC.ImgWrapper>
+                <RC.DescWrapper>
+                    <RC.span>nSuns</RC.span>
+                    <RC.ul>
+                        <RC.li>#diet</RC.li>
+                        <RC.li>#Female</RC.li>
+                        <RC.li>#6weeks</RC.li>
+                    </RC.ul>
+                    <div></div>
+                    <RC.DescFooterWrapper>
+                        <RC.CoachNameWrapper>
+                            <RC.CoachName>By r/Fitness</RC.CoachName>
+                        </RC.CoachNameWrapper>
+                        <RC.ClickWrapper>
+                            <RC.LikeIconWrapper>
+                                <Image src={LikeIcon} alt="좋아요아이콘" width={25} />
+                            </RC.LikeIconWrapper>
+                            <RC.BtnWrapper>
+                                <RC.AddBtn>추 가</RC.AddBtn>
+                            </RC.BtnWrapper>
+                        </RC.ClickWrapper>
+                    </RC.DescFooterWrapper>
+                </RC.DescWrapper>
+            </RC.CardWrapper>
+        </RC.CardBox>
+    );
+};
+
+export default RoutineCard;
