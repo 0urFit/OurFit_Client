1bb6686178b5cd27899bcfe184058343ba163e17
Kimyewon <yewon6282@gmail.com>
2023-04-09 23:50:09 +0900

    design: DefaultLayout 추가

---
+++ src/common/layout/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const DL = {
+    Container: styled.div`
+        margin: auto;
+        width: 24.375rem;
+        height: 52.75rem;
+        background-color: #bfbfbf;
+    `,
+};
+
+export { DL };
