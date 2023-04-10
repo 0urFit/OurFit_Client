2ef313a823a3ed4fe14b6992248bcb368f63a560
Kimyewon <yewon6282@gmail.com>
2023-04-11 03:13:03 +0900

    design: getLayout 적용

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -1,5 +1,20 @@
+import Image from 'next/image';
+import { DL } from '@/common/layout/style';
+import LeftArrowIcon from '../../public/assets/left-arrow-icon.png';
+
 const SignIn = () => {
     return <></>;
+};
+
+SignIn.getLayout = function getLayout(page: any) {
+    return (
+        <DL.PageLayout>
+            <DL.ImgWrapper>
+                <Image src={LeftArrowIcon} alt="left-arrow-icon" width="20" />
+            </DL.ImgWrapper>
+            <DL.FirstContainer>{page}</DL.FirstContainer>
+        </DL.PageLayout>
+    );
 };
 
 export default SignIn;
