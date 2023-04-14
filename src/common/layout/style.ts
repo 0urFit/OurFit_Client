585cefd6b4032e71df6f2bf721de7972e70c8f4e
Kimyewon <yewon6282@gmail.com>
2023-04-15 00:08:14 +0900

    design: 폰트 추가, 기타 css 수정

--- src/common/layout/style.ts
+++ src/common/layout/style.ts
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const DL = {
     PageLayout: styled.div`
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap');
+        font-family: 'Roboto', sans-serif;
         position: relative;
         width: 24.375rem;
         height: 52.75rem;
@@ -18,15 +20,17 @@ const DL = {
     FirstContainer: styled.div`
         display: inline-block;
         margin: 2.5rem 3rem;
-        min-width: calc(100% - 6rem);
-        min-height: calc(100% - 5rem);
+        width: calc(100% - 6rem);
+        height: calc(100% - 5rem);
+        overflow-y: scroll;
     `,
 
     SecondContainer: styled.div`
         display: inline-block;
         margin: 2.5rem 1.125rem 1rem;
-        min-width: calc(100% - 2.25rem);
-        min-height: calc(100% - 3.5rem);
+        width: calc(100% - 2.25rem);
+        height: calc(100% - 3.5rem);
+        overflow-y: scroll;
     `,
 };
 
