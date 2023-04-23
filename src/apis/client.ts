f2442b418d598a2f7339641575db6b4d26fc376a
seonjae <tjswo2292@naver.com>
2023-04-23 15:39:52 +0900

    feat: api instance 생성

---
+++ src/apis/client.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_URL = 'https://www.ourfit.shop';
+
+export const instance = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
