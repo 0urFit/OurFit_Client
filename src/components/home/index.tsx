15d6d5bb0227f958ed10269ba624fad92489b4c1
seonjae <tjswo2292@naver.com>
2023-05-05 15:47:51 +0900

    refactor: select-box 공통 컴포넌트 변경

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,49 +1,23 @@
 import Image from 'next/image';
 import { H } from './style';
-import OurfitLogo from '../../../public/assets/Ourfit_logo.png';
-import Select, { StylesConfig } from 'react-select';
-import React, { useId } from 'react';
+import OurfitLogo from '../../../public/assets/Ourfit_logo.svg';
+import React from 'react';
+import SmallSelect from '@/common/molecules/SmallSelect';
 
 const Home = () => {
     const test = [
         { value: 'all', label: 'all' },
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' },
+        { value: 'diet', label: 'diet' },
+        { value: 'strength', label: 'strength' },
+        { value: 'bodybuilding', label: 'bodybuilding' },
     ];
-
-    const customStlyes: StylesConfig = {
-        control: provided => ({
-            ...provided,
-            width: '6.25rem',
-            height: '1.875rem',
-            fontSize: '0.9375rem',
-            boxSizing: 'border-box',
-            minHeight: 0,
-        }),
-        dropdownIndicator: () => ({
-            display: 'none',
-        }),
-        indicatorsContainer: () => ({
-            display: 'none',
-        }),
-        placeholder: () => ({
-            fontWeight: 'bold',
-            color: '#000',
-        }),
-        option: (provided, { isFocused }) => ({
-            ...provided,
-            color: isFocused ? '#00A8E1' : 'black',
-        }),
-    };
-
     return (
         <>
             <H.LogoBox>
                 <Image src={OurfitLogo} width={100} height={20} alt="로고" />
             </H.LogoBox>
             <H.SelectBox>
-                <Select instanceId={useId()} placeholder="운동선택" options={test} styles={customStlyes} defaultValue={test[0]} />
+                <SmallSelect placeholder={'운동종목'} options={test} />
             </H.SelectBox>
         </>
     );
