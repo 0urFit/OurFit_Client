9f466c31d97bf7ca99c76d35c0a49b06d608235a
Kimyewon <yewon6282@gmail.com>
2023-05-08 18:56:49 +0900

    design: save strength 페이지 퍼블리싱

---
+++ src/components/save/detail/strength/index.tsx
@@ -0,0 +1,67 @@
+import { BB } from '../../style';
+import CreateButton from '@/common/molecules/CreateButton';
+
+const Strength = () => {
+    return (
+        <BB.Container>
+            <BB.TopBox>
+                <BB.TopWrapper>
+                    <BB.Title>nSuns</BB.Title>
+                    <ul>
+                        <BB.Tag>#diet</BB.Tag>
+                        <BB.Tag>#female</BB.Tag>
+                        <BB.Tag>#4weeks</BB.Tag>
+                    </ul>
+                </BB.TopWrapper>
+            </BB.TopBox>
+            <BB.BottomBox>
+                <BB.MainWrapper>
+                    <BB.ListWrapper>
+                        <BB.ExerciseTitle>Bench Press</BB.ExerciseTitle>
+                        <BB.Table>
+                            <thead>
+                                <tr>
+                                    <th>Set</th>
+                                    <th>Weight</th>
+                                    <th>Reps</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>1</th>
+                                    <th>100kg</th>
+                                    <th>10</th>
+                                    <th>
+                                        <input type="checkbox" />
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th>2</th>
+                                    <th>100kg</th>
+                                    <th>10</th>
+                                    <th>
+                                        <input type="checkbox" />
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th>3</th>
+                                    <th>100kg</th>
+                                    <th>10</th>
+                                    <th>
+                                        <input type="checkbox" />
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </BB.Table>
+                    </BB.ListWrapper>
+                </BB.MainWrapper>
+                <BB.ButtonWrapper>
+                    <CreateButton message='운동 완료' />
+                </BB.ButtonWrapper>
+            </BB.BottomBox>
+        </BB.Container>
+    );
+};
+
+export default Strength;
