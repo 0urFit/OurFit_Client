e0428e24ede126cc6a6be81c1c094836832b016a
seonjae <tjswo2292@naver.com>
2023-05-15 13:55:43 +0900

    feat: home 추가

--- src/components/home/style.ts
+++ src/components/home/style.ts
@@ -21,4 +21,88 @@ const H = {
     `,
 };
 
-export { H };
+const HD = {
+    RoutineTitleBox: styled.div`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 9rem;
+    `,
+    RoutineTitle: styled.span`
+        font-size: 1.875rem;
+        font-weight: bold;
+    `,
+    RoutineDescBox: styled.div`
+        position: relative;
+        width: 100%;
+        height: 43.75rem;
+        border: 0.0625rem solid #e1e2e3;
+        border-top-left-radius: 2.5rem;
+        border-top-right-radius: 2.5rem;
+        background-color: #ededed;
+    `,
+    OverviewBox: styled.div`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 3rem;
+        left: 1.125rem;
+        width: 22.125rem;
+        height: 8.5rem;
+        border-radius: 0.3125rem;
+        background-color: #fff;
+    `,
+    OverviewDataTable: styled.table`
+        width: 20.3125rem;
+        height: 6.5625rem;
+        text-align: left;
+        table-layout: fixed;
+    `,
+    Tbody: styled.tbody``,
+    Thead: styled.thead``,
+    Th: styled.th`
+        font-weight: 700;
+    `,
+    Tr: styled.tr``,
+    ContentTitle: styled.td`
+        vertical-align: middle;
+        font-size: 0.8125rem;
+    `,
+    Content: styled.td`
+        vertical-align: middle;
+        font-size: 0.8125rem;
+        color: #b4b4b4;
+    `,
+    JustSpacing: styled.td`
+        padding: 10px;
+    `,
+    RoutineSlideBox: styled.div`
+        display: flex;
+        position: absolute;
+        top: 12.1875rem;
+        width: inherit;
+        height: 26.125rem;
+        overflow-x: scroll;
+
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    `,
+    RoutineDetailBox: styled.div`
+        min-width: 14.625rem;
+        height: inherit;
+        margin-right: 15px;
+        border-radius: 0.3125rem;
+        background-color: #fff;
+
+        &:first-child {
+            margin-left: 1.125rem;
+        }
+    `,
+    PrevButton: styled.button``,
+    NextButton: styled.button``,
+};
+
+export { H, HD };
