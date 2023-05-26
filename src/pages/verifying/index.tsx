0263ff23a849c2d20b9c2f77ef80a04aa91cb443
seonjae <tjswo2292@naver.com>
2023-05-27 01:47:35 +0900

    feat: access token 갱신

--- src/pages/verifying/index.tsx
+++ src/pages/verifying/index.tsx
@@ -1,7 +1,37 @@
-import Loading from '@/components/loading';
+import { SocialKakaoLogin } from '@/apis/auth';
+import Loading, { VerifyingPagePropsType } from '@/components/loading';
+import wrapper from '@/store/store';
+import { GetServerSidePropsContext, GetServerSideProps, InferGetServerSidePropsType } from 'next/types';
 
-const VerifyingPage = () => {
-    return <Loading />;
+const VerifyingPage = (verifyingPageProps: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    return <Loading verifyingPageProps={verifyingPageProps} />;
 };
+
+export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(store => async (ctx: GetServerSidePropsContext) => {
+    const { code } = ctx.query;
+    const KAKAO_CODE = code as string;
+    const verifyingPageProps: VerifyingPagePropsType = {
+        accessToken: '',
+        refreshToken: '',
+        success: false,
+    };
+
+    try {
+        const response = await SocialKakaoLogin(KAKAO_CODE);
+
+        const { token, refreshToken } = response.data.result;
+        const { success } = response.data;
+
+        verifyingPageProps.accessToken = token;
+        verifyingPageProps.refreshToken = refreshToken;
+        verifyingPageProps.success = success;
+    } catch (error: any) {
+        console.log(error);
+    }
+
+    return {
+        props: verifyingPageProps,
+    };
+});
 
 export default VerifyingPage;
