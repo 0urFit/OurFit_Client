a8d8535d0b0c13fb170bb6c4357d856d613fae87
Kimyewon <yewon6282@gmail.com>
2023-06-16 15:44:58 +0900

    feat: routineSuccess api 추가

--- src/components/save/detail/index.tsx
+++ src/components/save/detail/index.tsx
@@ -1,38 +1,44 @@
 import { useEffect, useState } from 'react';
 import { SaveRoutineDetail, RoutineSuccess } from '@/apis/auth';
-import { useAppSelector } from '@/store/hook';
 import { BB } from '../style';
-import { ExercisesType, RoutineType } from '../type';
+import { RoutineType, TodayExercisesType } from '../type';
 import CreateButton from '@/common/molecules/CreateButton';
 import Pagination from './Pagination';
 import RestIcon from '../../../../public/assets/rest-icon.svg';
 import Image from 'next/image';
 import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
 
 const SaveDetail = () => {
-    const { register, reset, handleSubmit } = useForm<boolean[]>();
+    const router = useRouter();
+    const { routineId, weekProgress } = router.query;
+    const usedId = routineId as unknown as number;
+    const usedWeek = weekProgress as unknown as number;
 
     const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const todayDate = new Date();
-    const [currentWeek, setCurrentWeek] = useState<string>(week[todayDate.getDay()]);
+    const [currentDay, setCurrentWeek] = useState<string>(week[todayDate.getDay()]);
 
-    const selector = useAppSelector(state => state.routine);
+    const { register, reset, handleSubmit } = useForm<boolean[]>();
+
     const [routine, setRoutine] = useState<RoutineType>();
-    const [exercise, setExercise] = useState<ExercisesType[]>();
+    const [exercise, setExercise] = useState<TodayExercisesType>();
+    const [isLastDay, setIsLastDay] = useState<boolean>(false);
 
     useEffect(() => {
         reset();
-        const todayRoutine = routine?.days.filter(el => el.day === currentWeek)[0];
-        setExercise(todayRoutine?.exercises);
-    }, [currentWeek]);
+        setIsLastDay(routine?.days[routine.days.length - 1].day === currentDay);
+        const todayRoutine = routine?.days.filter(el => el.day === currentDay)[0];
+        setExercise(todayRoutine);
+    }, [routine, currentDay]);
 
     useEffect(() => {
         handleData();
-    }, []);
+    }, [usedId]);
 
     const handleData = async () => {
         try {
-            const result = await SaveRoutineDetail(selector.routineId, selector.week);
+            const result = await SaveRoutineDetail(usedId, usedWeek);
             setRoutine(result.data.result[0]);
         } catch (error) {
             console.log(error);
@@ -41,10 +47,10 @@ const SaveDetail = () => {
 
     const handleSuccess = async (data: boolean[]) => {
         const dataArr = Object.values(data);
-        const successRate = Math.round((dataArr.filter(el => el === true).length / dataArr.length) * 100);
+        const successRate = ((dataArr.filter(el => el === true).length / dataArr.length) * 100).toFixed(1) as unknown as number;
 
         try {
-            const result = await RoutineSuccess(selector.routineId, successRate);
+            const result = await RoutineSuccess(usedId, usedWeek, currentDay, successRate, isLastDay);
             console.log(result);
         } catch (error) {
             console.log(error);
@@ -55,57 +61,56 @@ const SaveDetail = () => {
         <BB.Container>
             <BB.TopBox>
                 <BB.TopWrapper>
-                    <BB.Title>{selector.routineName}</BB.Title>
+                    <BB.Title>{routine?.routineName}</BB.Title>
                     <BB.TagWrapper>
-                        <BB.Tag>#{selector.category}</BB.Tag>
-                        <BB.Tag>#{selector.fewTime}/week</BB.Tag>
-                        <BB.Tag>#{selector.period} weeks</BB.Tag>
+                        <BB.Tag>#{routine?.category}</BB.Tag>
+                        <BB.Tag>#{routine?.fewTime}/week</BB.Tag>
+                        <BB.Tag>#{routine?.period} weeks</BB.Tag>
                     </BB.TagWrapper>
                 </BB.TopWrapper>
             </BB.TopBox>
             <BB.BottomBox>
                 <BB.WeekWrapper>
-                    <Pagination currentWeek={currentWeek} setCurrentWeek={setCurrentWeek} />
+                    <Pagination currentDay={currentDay} setCurrentWeek={setCurrentWeek} />
                 </BB.WeekWrapper>
                 <form onSubmit={handleSubmit(handleSuccess)}>
                     <BB.MainBox>
                         <BB.MainWrapper>
-                            {exercise?.map((data, key) => (
-                                <BB.ListWrapper key={key}>
-                                    <BB.ExerciseTitle>
-                                        {key + 1}.&nbsp;{data.name}
-                                    </BB.ExerciseTitle>
-                                    <BB.Table>
-                                        <thead>
-                                            <tr>
-                                                <th>Set</th>
-                                                <th>Weight</th>
-                                                <th>Reps</th>
-                                                <th></th>
-                                            </tr>
-                                        </thead>
-                                        {data.sets.map(element => (
-                                            <tbody key={element.id}>
+                            {exercise ? (
+                                exercise?.exercises.map((data, key) => (
+                                    <BB.ListWrapper key={key}>
+                                        <BB.ExerciseTitle>
+                                            {key + 1}.&nbsp;{data.name}
+                                        </BB.ExerciseTitle>
+                                        <BB.Table>
+                                            <thead>
                                                 <tr>
-                                                    <th>{element.sequence}</th>
-                                                    <th>{element.weight}</th>
-                                                    <th>{element.reps}</th>
-                                                    <th>
-                                                        <input type="checkbox" {...register(`${element.id}`)} />
-                                                    </th>
+                                                    <th>Set</th>
+                                                    <th>Weight</th>
+                                                    <th>Reps</th>
+                                                    <th></th>
                                                 </tr>
-                                            </tbody>
-                                        ))}
-                                    </BB.Table>
-                                </BB.ListWrapper>
-                            ))}
-                            {exercise === undefined ? (
+                                            </thead>
+                                            {data.sets.map(element => (
+                                                <tbody key={element.id}>
+                                                    <tr>
+                                                        <th>{element.sequence}</th>
+                                                        <th>{element.weight}</th>
+                                                        <th>{element.reps}</th>
+                                                        <th>
+                                                            <input type="checkbox" {...register(`${element.id}`)} />
+                                                        </th>
+                                                    </tr>
+                                                </tbody>
+                                            ))}
+                                        </BB.Table>
+                                    </BB.ListWrapper>
+                                ))
+                            ) : (
                                 <BB.WarningWrapper>
                                     <Image src={RestIcon} alt="rest-icon" width={100} height={100} />
                                     <BB.WarningMessage>오늘 운동 루틴은 없는 날입니다!</BB.WarningMessage>
                                 </BB.WarningWrapper>
-                            ) : (
-                                ''
                             )}
                         </BB.MainWrapper>
                     </BB.MainBox>
