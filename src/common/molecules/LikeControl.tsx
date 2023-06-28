820820825e8d6544f124be609b508a71a3bd5c4a
seonjae <tjswo2292@naver.com>
2023-06-28 22:37:54 +0900

    refactor: 변수명, import 순서 수정

--- src/common/molecules/LikeControl.tsx
+++ src/common/molecules/LikeControl.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable indent */
 import Image from 'next/image';
+import { useState } from 'react';
+
+import { LikeIconClick, LikeIconUnclick } from '@/apis/auth';
+import getErrorMessage from '@/utils/getErrorMessage';
 
 import { LC } from './style';
 import LikeIcon from '../../../public/assets/like-icon.svg';
 import FilledLikeIcon from '../../../public/assets/filled-like-icon.svg';
-import { LikeIconClick, LikeIconUnclick } from '@/apis/auth';
-import { useState } from 'react';
 
 interface LikeControlType {
     id: number | undefined;
@@ -13,29 +15,29 @@ interface LikeControlType {
 }
 
 const LikeControl = ({ id, liked }: LikeControlType) => {
-    const [test, setTest] = useState(liked);
+    const [isLike, setIsLike] = useState(liked);
 
     const fetchLiked = async (routineId: number | undefined) => {
         try {
-            if (!test) {
+            if (!isLike) {
                 const response = await LikeIconClick(routineId);
                 const { result } = response.data;
 
-                setTest(result);
+                setIsLike(result);
             } else {
                 const response = await LikeIconUnclick(routineId);
                 const { result } = response.data;
 
-                setTest(result);
+                setIsLike(result);
             }
-        } catch (error) {
-            console.log(error);
+        } catch (e) {
+            throw new Error(getErrorMessage(e));
         }
     };
 
     return (
         <LC.LikeIconBtn onClick={() => fetchLiked(id)}>
-            {test ? <Image src={FilledLikeIcon} alt="채워진좋아요아이콘" fill={true} /> : <Image src={LikeIcon} alt="비어있는좋아요아이콘" fill={true} />}
+            {isLike ? <Image src={FilledLikeIcon} alt="채워진좋아요아이콘" fill={true} /> : <Image src={LikeIcon} alt="비어있는좋아요아이콘" fill={true} />}
         </LC.LikeIconBtn>
     );
 };
