b66217334456a96ca669ec2fb62a8a0414b5c294
seonjae <tjswo2292@naver.com>
2023-09-26 15:37:58 +0900

    feat: LikeControl id 값만로 동작

--- src/common/molecules/LikeControl.tsx
+++ src/common/molecules/LikeControl.tsx
@@ -2,34 +2,40 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
-import { LikeIconClick, LikeIconUnclick } from '@/apis/auth';
 import getErrorMessage from '@/utils/getErrorMessage';
 
 import { LC } from './style';
 import LikeIcon from '../../../public/assets/like-icon.svg';
 import FilledLikeIcon from '../../../public/assets/filled-like-icon.svg';
+import { LikeRequest } from '@/apis/likeRequest';
 
 interface LikeControlType {
     id: number | undefined;
-    liked: boolean;
     handleLikeList?: () => void;
 }
 
-const LikeControl = ({ id, liked, handleLikeList }: LikeControlType) => {
-    const [isLike, setIsLike] = useState(liked);
+const LikeControl = ({ id, handleLikeList }: LikeControlType) => {
+    const [isLike, setIsLike] = useState(false);
 
-    const fetchLiked = async (routineId: number | undefined) => {
+    const handleLiked = async (routindId: number | undefined) => {
+        const likeRequest = new LikeRequest(routindId);
+
         try {
-            if (!isLike) {
-                const response = await LikeIconClick(routineId);
-                const { result } = response.data;
+            switch (isLike) {
+                case false: {
+                    const response = await likeRequest.LikePost();
+                    const { result } = response.data;
 
-                setIsLike(result);
-            } else {
-                const response = await LikeIconUnclick(routineId);
-                const { result } = response.data;
+                    setIsLike(result);
+                    break;
+                }
+                case true: {
+                    const response = await likeRequest.LikeDelete();
+                    const { result } = response.data;
 
-                setIsLike(result);
+                    setIsLike(result);
+                    break;
+                }
             }
         } catch (e) {
             throw new Error(getErrorMessage(e));
@@ -37,11 +43,27 @@ const LikeControl = ({ id, liked, handleLikeList }: LikeControlType) => {
     };
 
     useEffect(() => {
+        const fetchLiked = async () => {
+            const likeRequest = new LikeRequest(id);
+
+            try {
+                const response = await likeRequest.LikeGet();
+                const { result } = response.data;
+                setIsLike(result);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchLiked();
+    }, []);
+
+    useEffect(() => {
         handleLikeList?.();
     }, [isLike]);
 
     return (
-        <LC.LikeIconBtn onClick={() => fetchLiked(id)}>
+        <LC.LikeIconBtn onClick={() => handleLiked(id)}>
             {isLike ? <Image src={FilledLikeIcon} alt="채워진좋아요아이콘" fill={true} /> : <Image src={LikeIcon} alt="비어있는좋아요아이콘" fill={true} />}
         </LC.LikeIconBtn>
     );
