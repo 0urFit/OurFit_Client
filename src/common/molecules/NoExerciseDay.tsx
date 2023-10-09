7f9a62014150b82e74a29d90f0984adc1e914009
seonjae <tjswo2292@naver.com>
2023-10-09 21:22:53 +0900

    feat: 휴식 요일 컴포넌트 추가

---
+++ src/common/molecules/NoExerciseDay.tsx
@@ -0,0 +1,11 @@
+import { NE } from './style';
+
+const NoExerciseDay = () => {
+    return (
+        <NE.Box>
+            <NE.Title>운동보다 중요한 휴식날 입니다.</NE.Title>
+        </NE.Box>
+    );
+};
+
+export default NoExerciseDay;
