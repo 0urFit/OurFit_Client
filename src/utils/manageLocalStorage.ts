63fcc8333de8ac20a2c202331426d4e294437946
seonjae <tjswo2292@naver.com>
2023-10-11 16:11:58 +0900

    chore: refresh token, access token 브라우저 쿠키 키값 상수화

--- src/utils/manageLocalStorage.ts
+++ src/utils/manageLocalStorage.ts
@@ -1,11 +1,13 @@
+import { TOKEN } from './constants';
+
 export const manageAccessToken = {
     GET: () => {
-        return localStorage.getItem('access_token');
+        return localStorage.getItem(TOKEN.ACCESS_TOKEN_KEY);
     },
     SET: (token: string) => {
-        return localStorage.setItem('access_token', token);
+        return localStorage.setItem(TOKEN.ACCESS_TOKEN_KEY, token);
     },
     REMOVE: () => {
-        return localStorage.removeItem('access_token');
+        return localStorage.removeItem(TOKEN.ACCESS_TOKEN_KEY);
     },
 };
