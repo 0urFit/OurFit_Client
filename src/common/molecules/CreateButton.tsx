7f916b6c85fc8469818c028d83ff0e4a0cb43081
seonjae <tjswo2292@naver.com>
2023-10-12 12:24:15 +0900

    feat: 등록된 운동루틴 기능 추가

--- src/common/molecules/CreateButton.tsx
+++ src/common/molecules/CreateButton.tsx
@@ -3,10 +3,15 @@ import { CB } from './style';
 interface propsType {
     message: string;
     handleSubmit?: () => void;
+    isSaved?: boolean;
 }
 
-const CreateButton = ({ message, handleSubmit }: propsType) => {
-    return <CB.Button onClick={handleSubmit}>{message}</CB.Button>;
+const CreateButton = ({ message, handleSubmit, isSaved }: propsType) => {
+    return (
+        <CB.Button disabled={isSaved} onClick={handleSubmit} $isSaved={isSaved}>
+            {message}
+        </CB.Button>
+    );
 };
 
 export default CreateButton;
