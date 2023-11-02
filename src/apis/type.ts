6bde8e93799af138576bf49758bfa06575a2a669
seonjae <tjswo2292@naver.com>
2023-11-02 11:59:26 +0900

    feat: GetRoutin api 반환 타입 지정

--- src/apis/type.ts
+++ src/apis/type.ts
@@ -10,3 +10,14 @@ export type LoginApiType = {
     email: string;
     password: string;
 };
+
+export type GetRoutineType = {
+    id: number;
+    imgpath: string;
+    period: number;
+    liked: boolean;
+    enrolled: boolean;
+    fewTime: number;
+    routineName: string;
+    category: string;
+};
