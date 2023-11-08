21fc760d35fab6837cb318414f5b897505e79b44
seonjae <tjswo2292@naver.com>
2023-11-08 16:09:15 +0900

    refactor: "추가" 기능은 CreateButton 컴포넌트로 책임부여

--- src/common/molecules/CreateButton.tsx
+++ src/common/molecules/CreateButton.tsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import { SaveRoutineInfo } from '@/apis/apiService';
+import getErrorMessage from '@/utils/getErrorMessage';
+
 import { CB } from './style';
 
 interface propsType {
     message: string;
-    handleSubmit?: () => void;
-    isSaved?: boolean;
+    isEnrolled?: boolean;
+    routindId: number;
 }
 
-const CreateButton = ({ message, handleSubmit, isSaved }: propsType) => {
+const CreateButton = ({ message, isEnrolled, routindId }: propsType) => {
+    const [isSaved, setIsSaved] = useState<boolean | undefined>();
+
+    const handleSaveRoutine = async () => {
+        try {
+            const response = await SaveRoutineInfo(routindId);
+            const { success } = response.data;
+            setIsSaved(success);
+        } catch (e) {
+            throw new Error(getErrorMessage(e));
+        }
+    };
+
+    useEffect(() => {
+        setIsSaved(isEnrolled);
+    }, [isEnrolled]);
+
     return (
-        <CB.Button disabled={isSaved} onClick={handleSubmit} $isSaved={isSaved}>
+        <CB.Button disabled={isSaved} onClick={handleSaveRoutine} $isSaved={isSaved}>
             {message}
         </CB.Button>
     );
